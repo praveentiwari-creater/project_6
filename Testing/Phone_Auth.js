5ea7c1ff684656356f1a34ba7e1b9c8f55e97796
Praveen Tiwari <sp2tiwari@gmail.com>
2020-07-09 10:14:41 +0530

    project 6

---
+++ Testing/Phone_Auth.js
@@ -0,0 +1,23 @@
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
+  } from 'react-native';
+  
+  
+  class Phone_Auth extends React.Component {
+      render(){
+    return (
+     <View>
+       <Text style={{fontSize:40}}>
+         test
+       </Text>
+     </View>
+    );
+  };
+  }
+export default Phone_Auth;
+  
